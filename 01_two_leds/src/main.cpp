45277c35246d1d61c40da9534ff0504d66b70c0d
danielblanco96 <dablancal96@gmail.com>
2022-07-23 12:30:04 +0100

    Move project to platformio

--- 01_two_leds/src/main.cpp
+++ 01_two_leds/src/main.cpp
@@ -1,3 +1,5 @@
+#include <Arduino.h>
+
 const int greenLedPin = 23;
 const int redLedPin = 22;
 
@@ -9,12 +11,12 @@ void setup() {
 void loop() {
   digitalWrite(redLedPin, HIGH);
   digitalWrite(greenLedPin, HIGH);
-  delay(500);
+  delay(1500);
   digitalWrite(redLedPin, LOW);
-  delay(500);
+  delay(1500);
   digitalWrite(redLedPin, HIGH);
   digitalWrite(greenLedPin, LOW);
-  delay(500);
+  delay(1500);
   digitalWrite(redLedPin, LOW);
-  delay(500);
+  delay(1500);
 }
